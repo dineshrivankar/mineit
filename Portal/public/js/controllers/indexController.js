7fef82a87a31971001e03244eb06bc7bc13dfac6
Amol Pednekar <amol_pednekar@persistent.com>
2017-11-13 11:37:02 +0530

    Login controller binding

--- Portal/public/js/controllers/indexController.js
+++ Portal/public/js/controllers/indexController.js
@@ -1,16 +1,35 @@
 
 var pslgame = angular.module('pslgame');
 
-pslgame.controller('indexController', function($scope , $http) {
+pslgame.controller('indexController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
 
-$scope.login = function (){
-	alert("Login");	
-}
+	$scope.login = function () {
+
+		$rootScope.username = $scope.username
+		console.log("Username in rootScope.username", $rootScope.username);
+
+		var url = apiUrl + '/users/register';
+		data = { username: $rootScope.username }
+
+		$http.post(url, data).success(function (response) {
+			console.log("Login success response", response);
+			// TODO - Redirect to dashboard
+		}).catch(function (response) {
+			console.log("Login failed response!", response);
+			// TODO - Show error on login
+		})
+	}
 
 
-$scope.generateUserName = function (){
-	alert("generateUserName");	
-}
-});
+	$scope.generateUserName = function () {
+		var url = apiUrl + '/users/generate/names';
 
+		$http.get(url).success(function (response) {
+			console.log("generateUserName success response", response);
+			$scope.username = JSON.parse(response);
+		}).catch(function (response) {
+			console.log("generateUserName failed response!", response);
+		})
+	}
 
+}]);
